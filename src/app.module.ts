b1914135601e0af31f96f310697ea2fb9c3ea104
Marlon Panerari <marpan.dev@gmail.com>
2026-04-08 11:10:10 -0300

    CRUD loja de games - NestJS Mysql

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categoria/categoria.module';
+import { ProdutoModule } from './produto/produto.module';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { Produto } from './produto/entities/produto.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_gamestore',
+      entities: [Produto, Categoria],
+      synchronize: true,
+    }),
+    CategoriaModule,
+    ProdutoModule,
+  ],
+})
+export class AppModule {}
